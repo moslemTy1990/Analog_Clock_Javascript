7597f7b10e440e2745c38284f63e61cbec434b2f
Moslem <m.teymoori.1990@gmail.com>
2020-08-13 00:39:13 +0200

    placing the clock image on the page!

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-console.log('8');
+const HOURHAND = document.querySelector("#hour");
+const MINUTEHAND = document.querySelector("#minute");
+const SECONDHAND = document.querySelector("#second");
+
+let hrPos = 20;
+let minPos = 130;
+let secPos = 267;
+
+var date = new Date();
+
+HOURHAND.style.transform = "rotate(" + date.getHours() + "deg)";
+MINUTEHAND.style.transform = "rotate(" + date.getMinutes() + "deg)";
+SECONDHAND.style.transform = "rotate(" + date.getSeconds() + "deg)";
