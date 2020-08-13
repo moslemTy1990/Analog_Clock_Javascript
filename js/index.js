d655be497f4741d6ddbe3450de919abbbcd0a88c
Moslem <m.teymoori.1990@gmail.com>
2020-08-13 18:55:49 +0200

    final testing of an analog clock!

--- js/index.js
+++ js/index.js
@@ -2,12 +2,25 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-let hrPos = 20;
-let minPos = 130;
-let secPos = 267;
 
 var date = new Date();
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getSeconds();
 
-HOURHAND.style.transform = "rotate(" + date.getHours() + "deg)";
-MINUTEHAND.style.transform = "rotate(" + date.getMinutes() + "deg)";
-SECONDHAND.style.transform = "rotate(" + date.getSeconds() + "deg)";
+let hrPos = (hr * 360 / 12) + (min * (360 / 60) / 12);
+let minPos = (min * 360 / 60) + (sec * (360 / 60) / 60);
+let secPos = sec * 360 / 60;
+
+function runTheClock() {
+    hrPos = hrPos + (3/360);
+    minPos = minPos + (6/60);
+    secPos = secPos + 6;
+
+
+    HOURHAND.style.transform = "rotate(" + hrPos + "deg)";
+    MINUTEHAND.style.transform = "rotate(" + minPos + "deg)";
+    SECONDHAND.style.transform = "rotate(" + secPos + "deg)";
+}
+
+var interval = setInterval(runTheClock,1000)
